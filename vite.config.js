9e2776e701a779a051e4dcf16eb45621d0b00de7
sengwoong <92924243+sengwoong@users.noreply.github.com>
2025-09-04 13:06:47 +0900

    asfksa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Railway 루트 배포니까 base는 "/" 고정
+  build: {
+    outDir: 'dist'
+  },
+  preview: {
+    port: 3000,
+    strictPort: true
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  }
 })
